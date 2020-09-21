e51e7adf37b85e887b5e5cd645196933ac2d6755
ETProduction_ThomasKirouac <thomaskirouac99@gmail.com>
2020-09-21 16:09:57 -0400

    Tile map template + camera follow player

---
+++ GameJam Collectible 2020-09-20/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float m_speed;
+    [SerializeField] private Animator m_Animator;
+    private Vector3 m_DirectionX = new Vector3(1, 0, 0);
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
+        m_Animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
+        if (m_Animator.GetFloat("Vertical") > -0.001 && m_Animator.GetFloat("Vertical") < 0.001)
+        {
+            m_Animator.SetBool("MovingVertical",false);
+        }
+        else
+        {
+            m_Animator.SetBool("MovingVertical", true);
+        }
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        transform.Translate(movement.normalized* Time.deltaTime * m_speed);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("test");
+    }
+}
