0c177ccaeb7bcf854952483c219ee1bf50fd88ce
ETProduction_ThomasKirouac <thomaskirouac99@gmail.com>
2020-09-25 12:14:25 -0400

    Minnor bug fix

---
+++ GameJam Collectible 2020-09-20/Assets/Scripts/MainMenu/BtnExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BtnExit : MonoBehaviour
+{
+    [SerializeField] private Button exitbtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        exitbtn.onClick.AddListener(onbtnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void onbtnClick()
+    {
+        Application.Quit();
+    }
+}
