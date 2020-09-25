31f33aa849801fa5722d6b8a2452ebb200cd4089
ETProduction_ThomasKirouac <thomaskirouac99@gmail.com>
2020-09-25 12:19:51 -0400

    remove unuse library

--- GameJam Collectible 2020-09-20/Assets/Scripts/PlayerBehavior.cs
+++ GameJam Collectible 2020-09-20/Assets/Scripts/PlayerBehavior.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.Experimental.AssetImporters;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
